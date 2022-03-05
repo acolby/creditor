835eab771e9a8280fef9c0ce258d731b6e7877f3
Aaron Colby <acolby@live.com>
2022-03-05 07:20:25 -0800

    file extentions in pattern usage parser

--- src/utils/analyzeSrc/parsePatternUsage/index.js
+++ src/utils/analyzeSrc/parsePatternUsage/index.js
@@ -1,5 +1,5 @@
 
-function utils_analyzeSrc_parsePatternUsage({ templates }, string) {
+function utils_analyzeSrc_parsePatternUsage({ templates }, string, matches = []) {
 
   const delimeters = {
     '_': true,
@@ -8,31 +8,52 @@ function utils_analyzeSrc_parsePatternUsage({ templates }, string) {
     '-': true,
   };
 
-  const terminators = [ '/index', '(', ' ', '\'', '"', '`', '  ', '\n' ];
+  const terminators = {
+    '(': true,
+    ' ': true,
+    '\'': true,
+    '"': true,
+    '`': true,
+    '  ': true,
+    '\n': true,
+    '}': true,
+    '\r': true,
+    '.': true,
+    ';': true,
+  };
 
   // one pattern per line for now
   let match = null;
   Object.keys(templates)
   .forEach((templatesName) => {
     const col_start = string.indexOf(templatesName);
+    const delimeter = string[col_start + templatesName.length];
     // console.log('col_start', templatesName, string, col_start)
 
-    if (col_start > 0) {
-      let col_end = templatesName.length + col_start;
-      const delimeter = string[col_end];
+    if (col_start > 0 && delimeters[delimeter]) {
+      // pattern was found
+      let col_end = col_start;
+      let terminator = false;
+      while (!terminator && string[col_end]) {
+        col_end++;
+        const char = string[col_end];
+        terminator = (terminators[char] && char !== delimeter) && char;
+      }
 
-      if (!delimeters[delimeter]) return;
+      const items = `${string.slice(col_start, col_end)}`.split(delimeter);
+      if (delimeter === '/' && terminator  === '.') {
+        items.pop();
+      }
 
-      const substirng = string.slice(col_end);
-      terminators.forEach((terminator) => {
-        const loc = substirng.indexOf(terminator);
-        if (loc > -1 && loc < col_end) col_end = loc
-      });
+      matches.push(items.join('/'));
 
-      match = `${templatesName}${substirng.slice(0, col_end)}`.split(delimeter).join('/')
+      if (!!string[col_end]) {
+        utils_analyzeSrc_parsePatternUsage({ templates }, string.slice(col_end), matches)
+      }
     }
   });
-  return match;
+
+  return matches;
 }
 
 module.exports = utils_analyzeSrc_parsePatternUsage;
--- src/utils/analyzeSrc/parsePatternUsage/test.js
+++ src/utils/analyzeSrc/parsePatternUsage/test.js
@@ -2,8 +2,53 @@ const expect = require('chai').expect;
 const mod = require('./');
 
 describe('utils_analyzeSrc_parsePatternUsage', () => {
-  it('should find a usage patten in a given string', () => {
-    const res = mod({ templates: { 'dodos': {} }}, 'export const dodos_main_item()');
-    expect(res).to.equal('dodos/main/item')
-  })
+  it('should parse a wide variety of strings', () => {
+
+    const expected = {
+      'export const dodos_main_item()': ['dodos/main/item'],
+      'import {dodos_user} from "#src/dodos/user"': ['dodos/user'],
+      'await dodos_user.actions.fetch("userId");': ['dodos/user'],
+      'from "#src/dodos/user"': ['dodos/user'],
+    };
+
+    Object.entries(expected)
+    .forEach(([given, output]) => {
+      const res = mod({ templates: { 'dodos': {} }}, given);
+      expect(res[0]).to.equal(output[0])
+    })
+    
+  });
+
+  it('should return multiple matches', () => {
+
+    const expected = {
+      'import {dodos_user} from "#src/dodos/user"': ['dodos/user', 'dodos/user'],
+      'import {dodos_user} from "#src/dingos/user/access"': ['dodos/user', 'dingos/user/access'],
+    };
+
+    Object.entries(expected)
+    .forEach(([given, output]) => {
+      const res = mod({ templates: { 'dodos': {}, 'dingos': {}, }}, given);
+      expect(res[0]).to.equal(output[0])
+      expect(res[1]).to.equal(output[1])
+    })
+    
+  });
+
+  it('should properly remove files if the delimiter is /', () => {
+
+    const expected = {
+      'import {dodos_user} from "#src/dodos/user/index.js"': ['dodos/user', 'dodos/user'],
+      'const dingos_user_access = requrie("#src/dingos/user/access/index.js")': ['dingos/user/access', 'dingos/user/access'],
+    };
+
+    Object.entries(expected)
+    .forEach(([given, output]) => {
+      const res = mod({ templates: { 'dodos': {}, 'dingos': {}, }}, given);
+      expect(res[0]).to.equal(output[0])
+      expect(res[1]).to.equal(output[1])
+    })
+    
+  });
+
 });
